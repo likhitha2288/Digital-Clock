f909dca85bb2cc359f66961f6b5e552e928432f5
likhitha2288 <likhithathota07@gmail.com>
2024-10-23 09:54:11 +0530

    Add files via upload

---
+++ Analog clock/script.js
@@ -0,0 +1,19 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+    const digitalClock = document.getElementById('digital-clock');
+    const formattedTime = d.toLocaleTimeString('en-US', { hour12: false });
+    digitalClock.textContent = formattedTime;
+
+})
+setInterval(updateClock, 1000);
+updateClock();
